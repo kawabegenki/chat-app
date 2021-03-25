632c07dd587ed117172443c4eff660f1d8ab6e13
kawabegenki <kwbgnk.work@gmail.com>
2021-03-25 18:16:37 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  #空白はNG
   validates :name, presence: true
+  #アソシエーション
+  has_many :room_users
+  has_many :rooms, through: :room_users
 end
